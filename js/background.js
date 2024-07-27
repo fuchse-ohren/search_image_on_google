d9e9f4c7675209673c08876872404c558e3a3fc5
fox ears <anonymous>
2024-07-28 01:02:04 +0900

    v1.1

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 browser.contextMenus.create({
     id: "AEC6128A-8B07-4224-B938-C2AF6FEC2EA6",
-    title: "Googleで画像を検索",
+    title: "GoogleLensで画像検索",
     contexts: ["image"],
     icons:{
         "16":"img/icon_16x16.png"
@@ -10,7 +10,7 @@ browser.contextMenus.create({
 browser.contextMenus.onClicked.addListener(function(info,tab) {
     if(info.menuItemId == "AEC6128A-8B07-4224-B938-C2AF6FEC2EA6"){
         var newTabParams = {
-            url: "https://www.google.co.jp/searchbyimage?image_url=" + encodeURI(info.srcUrl),
+            url: "https://lens.google.com/uploadbyurl?url=" + encodeURI(info.srcUrl),
             active: true
         };
     }
