688ef6779376f8ca435f2936f32341bffd2e441d
fuchse-ohren <git@kitsune.aleeas.com>
2026-02-28 14:11:53 +0900

    新しいタブを今のタブの隣に開くように設定

--- js/background.js
+++ js/background.js
@@ -9,11 +9,33 @@ browser.contextMenus.create({
 
 browser.contextMenus.onClicked.addListener(function(info,tab) {
     if(info.menuItemId == "AEC6128A-8B07-4224-B938-C2AF6FEC2EA6"){
+		console.log(info)
+		console.log(tab)
         var newTabParams = {
             url: "https://lens.google.com/uploadbyurl?url=" + encodeURI(info.srcUrl),
             active: true
+            index: tab.index + 1; // Thanks for theo1996.
         };
     }
     var creating = browser.tabs.create(newTabParams);
 }
 )
+
+
+/* 
+var image = document.getElementsByTagName("img")[0];
+
+var vcanv = document.createElement("canvas");
+var context = vcanv.getContext('2d');
+vcanv.width = image.naturalWidth;
+vcanv.height = image.naturalHeight;
+
+context.drawImage(image, 0, 0);
+
+var b64 = vcanv.toDataURL("image/png");
+var base64Data = b64.split(',')[1];
+
+let boundary = '----WebKitFormBoundary' + Math.random().toString(36).substring(7);
+
+console.log(b64)
+*/
